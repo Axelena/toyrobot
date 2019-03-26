4996e5739158fdfac47d8a4579749bb1fade4c59
Axelena <axelena@yandex.ru>
2019-03-26 11:50:48 +1100

    commit2

--- toyrobot.rb
+++ toyrobot.rb
@@ -5,8 +5,8 @@ require_relative 'lib/table'
 require_relative 'lib/command_processor'
 
 table = Table.new
-robot = Robot.new(Table.new)
-command_processor = CommandProcessor.new(robot)
+robot = Robot.new(table)
+command_processor = CommandProcessor.new(robot, table)
 #instruction_processor = InstructionProcessor.new#(command_factory)
 
 ARGF.each_line { |line| 
